aaecc9cd9e375b8f9cef3794fe4b4bf100b1ff67
анечка <144057821+RykunovaAnna@users.noreply.github.com>
2025-09-27 12:28:38 +0300

    Task 4
    Была добавлена валидация для сеттеров квалификации и специальностей

--- doctor.py
+++ doctor.py
@@ -1,3 +1,4 @@
+import json
 import re
 from typing import Any
 
@@ -52,7 +53,7 @@ class Doctor:
 
     @specialties.setter
     def specialties(self, specialties: list[Specialty]) -> None:
-        self.__specialties = specialties
+        self.__specialties = Doctor.validate_specialties(specialties)
 
     @property
     def qualification(self) -> Qualification:
@@ -60,7 +61,7 @@ class Doctor:
 
     @qualification.setter
     def qualification(self, qualification: Qualification) -> None:
-        self.__qualification = qualification
+        self.__qualification = Doctor.validate_qualification(qualification)
 
     @staticmethod
     def validate_name(name: str, name_type: str) -> str:
@@ -107,3 +108,116 @@ class Doctor:
             raise TypeError('Параметр qualification должен быть типа Qualification')
 
         return qualification
+
+    @staticmethod
+    def parse_init_data(*args, **kwargs) -> dict:
+        if len(args) == 1 and isinstance(args[0], Doctor):
+            data = Doctor.parse_init_doctor(args[0])
+        elif len(args) == 1 and isinstance(args[0], dict):
+            data = Doctor.parse_init_dict(args[0])
+        elif len(args) == 1 and isinstance(args[0], str):
+            if args[0].startswith('{') and args[0].endswith('}'):
+                data = Doctor.parse_init_json(args[0])
+            else:
+                data = Doctor.parse_init_string(args[0])
+        elif len(args) == 4:
+            data = Doctor.build_init_data(args[0], args[1], None, args[2], args[3])
+        elif len(args) == 5:
+            data = Doctor.build_init_data(args[0], args[1], args[2], args[3], args[4])
+        elif 1 < len(args) < 4 or len(args) > 6:
+            raise AttributeError('Не соответствует количество аттрибутов')
+        elif kwargs.get('doctor') and isinstance(kwargs.get('doctor'), Doctor):
+            data = Doctor.parse_init_doctor(kwargs.get('doctor'))
+        elif kwargs.get('dictionary') and isinstance(kwargs.get('dictionary'), dict):
+            data = Doctor.parse_init_dict(kwargs.get('dictionary'))
+        elif kwargs.get('json') and isinstance(kwargs.get('json'), dict):
+            data = Doctor.parse_init_json(kwargs.get('json'))
+        elif kwargs.get('string') and isinstance(kwargs.get('string'), str):
+            data = Doctor.parse_init_string(kwargs.get('string'))
+        else:
+            data = Doctor.parse_init_dict(kwargs)
+
+        return data
+
+    @staticmethod
+    def parse_init_doctor(doctor: "Doctor") -> dict:
+        return Doctor.build_init_data(
+            doctor.surname, doctor.firstname, doctor.patronymic, Qualification(doctor.qualification.title),
+            [Specialty(specialty.title) for specialty in doctor.specialties]
+        )
+
+    @staticmethod
+    def parse_init_dict(init_dict: dict) -> dict:
+        if set(init_dict.keys()) - {'surname', 'firstname', 'patronymic', 'qualification', 'specialties'}:
+            raise KeyError('Переданные ключи не соответствуют')
+
+        return init_dict
+
+    @staticmethod
+    def parse_init_string(init_string: str) -> dict:
+        data = [parameter.strip() if ',' not in parameter else parameter.strip().split(',')
+                for parameter in init_string.split(';')]
+        if 1 < len(data) < 4 or len(data) > 6:
+            raise AttributeError('Не соответствует количество аттрибутов')
+        if not isinstance(data[-1], list):
+            raise TypeError('Последний параметр строки должен быть специальностями')
+        qualification, specialities = Qualification(data[-2]), [Specialty(speciality) for speciality in data[-1]]
+
+        if len(data) == 4:
+            return Doctor.build_init_data(data[0], data[1], None, qualification, specialities)
+        elif len(data) == 5:
+            return Doctor.build_init_data(data[0], data[1], data[2], qualification, specialities)
+        else:
+            raise AttributeError('В строке указано неверное количество аргументов')
+
+    @staticmethod
+    def parse_init_json(init_json: str) -> dict:
+        data = json.loads(init_json)
+        prepared_dict = {}
+        for key in data:
+            if key == 'qualification':
+                if not isinstance(data[key], str):
+                    raise ValueError('В JSON в поле qualification должна храниться строка с название квалификации')
+
+                prepared_dict[key] = Qualification(data[key])
+            elif key == 'specialties':
+                if not isinstance(data[key], list):
+                    raise ValueError('В JSON в поле specialties должен храниться список с названием специальностей')
+
+                prepared_dict[key] = [Specialty(specialty) for specialty in data[key]]
+            else:
+                prepared_dict[key] = data[key]
+
+        return Doctor.parse_init_dict(prepared_dict)
+
+    @staticmethod
+    def build_init_data(surname: str, firstname: str, patronymic: str | None,
+                        qualification: Qualification, specialties: list[Specialty]) -> dict:
+        return {
+            'surname': surname,
+            'firstname': firstname,
+            'patronymic': patronymic,
+            'qualification': qualification,
+            'specialties': specialties,
+        }
+
+    def __str__(self):
+        patronymic_name = f'Отчество: {self.patronymic}\n' if self.patronymic else '\n'
+
+        return (f'Фамилия: {self.surname}\n'
+                f'Имя: {self.firstname}'
+                f'{patronymic_name}'
+                f'Квалификация: {self.qualification}\n'
+                f'Специальности: {", ".join(map(str, self.specialties))}\n')
+
+    @property
+    def short_str(self):
+        patronymic_name = f' {self.patronymic}' if self.patronymic else ''
+
+        return f'{self.surname} {self.firstname}{patronymic_name}'
+
+    def __eq__(self, other: Any) -> bool:
+        return isinstance(other, Doctor) and self.surname == other.surname and self.firstname == other.firstname and \
+               self.patronymic == other.patronymic and self.qualification == self.qualification and \
+               all(specialty in other.specialties for specialty in self.specialties) and \
+               all(specialty in self.specialties for specialty in other.specialties)
